701a96a90f1dbfddda535aa4a44878f4f7d4a493
Arvin-tc <arvinnliutc@gmail.com>
2025-01-20 18:44:57 -0800

    add the server file

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
+//import authRoutes from './routes/authRoutes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+//app.use('/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
